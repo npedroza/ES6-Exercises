8a7c59890286e96840834dbf0bf89a752c48f5ad
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-12-04 13:45:10 -0600

    Update E2.js

--- E2.js
+++ E2.js
@@ -1,17 +1,21 @@
-function reverse(a){
-    let l = a.length;
+function reverse(){
+    let l = this.length;
     let n = "";
     for (let v=l-1; v>=0; v--){
-      n += a[v]
+      n += this[v]
     }
     return n;
   }
-  
-  function reverseAll(b){
-    let strings = b.split(" ");
-    let words = strings.map((s) => {return reverse(s)}).join(" ")
-    return words;
-  }
-  let str = "hello world"
-  reverse(str) //'dlrow olleh'
-  reverseAll(str) //'olleh dlrow'
+  String.prototype.reverse = reverse;
+
+function reverseWords(){
+  let strings = this.split(" ");
+  let words = strings.map((s) => {return s.reverse()}).join(" ")
+  return words;
+}
+ String.prototype.reverseWords = reverseWords;
+
+
+let str = "hello world"
+str.reverse() //'dlrow olleh'
+str.reverseWords() //'olleh dlrow'
