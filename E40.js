86c5b72ad0136c4d4f18215e1a3e6aa1d180dfaa
nainpedroza <88448255+nainpedroza@users.noreply.github.com>
2021-12-08 13:12:58 -0600

    Update E40.js

--- E40.js
+++ E40.js
@@ -1,15 +1,31 @@
-function sumZero(array){
-    let sum = [];
-    for (let i = 0; i < array.length; i++){
-        for (let j= i+1; j < array.length; j++){
-            for (let k = j+1; k<array.length; k++){
-                if(array[i] + array[j] + array[k] === 0){
-                    sum.push([array[i], array[j], array[k]]);
-                }
-            }
-        }
+function sumZero(arr, sorting=false){
+  if(sorting) {
+    arr = arr.sort() //If we use sorting we can get non-repeated combinations of numbers
+  }
+  let newArr = []
+  for (let i=0; i<arr.length-1; i++){
+    let sum=arr[i]
+    for (let j=i+1; j<arr.length;j++){
+      sum = arr[i]+arr[j]
+      if (arr.includes(-sum,j+1)){
+        newArr.push([arr[i],arr[j],-sum])
+      }
     }
-    return sum;
+  }
+  return newArr
 }
 
+
+//Without sorting
 sumZero([3,2,1,0,-3,2,2,-2])
+/* [
+  [ 3, 0, -3 ],
+  [ 2, 1, -3 ],
+  [ 2, 0, -2 ],
+  [ 1, -3, 2 ],
+  [ 0, 2, -2 ],
+  [ 0, 2, -2 ]
+] */
+
+//With sorting
+sumZero([3,2,1,0,-3,2,2,-2],true) // [ [ -2, 0, 2 ], [ -3, 0, 3 ], [ -3, 1, 2 ] ]
